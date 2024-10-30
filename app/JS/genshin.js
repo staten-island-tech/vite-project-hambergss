2e14d56ea440a6e4270e7ce6b4a01299fd94d49f
hambergss <144480774+hambergss@users.noreply.github.com>
2024-10-30 13:54:59 -0400

    g

---
+++ app/JS/genshin.js
@@ -0,0 +1,82 @@
+const genshinCharacters = [
+  {
+    name: "Lumine",
+    element: "Anemo",
+    image: "https://example.com/lumine.jpg",
+    altText: "Lumine, the Traveler from Genshin Impact",
+  },
+  {
+    name: "Aether",
+    element: "Geo",
+    image: "https://example.com/aether.jpg",
+    altText: "Aether, the Traveler from Genshin Impact",
+  },
+  {
+    name: "Amber",
+    element: "Pyro",
+    image: "https://example.com/amber.jpg",
+    altText: "Amber, the Outrider of Mondstadt",
+  },
+  {
+    name: "Venti",
+    element: "Anemo",
+    image: "https://example.com/venti.jpg",
+    altText: "Venti, the Anemo Archon",
+  },
+  {
+    name: "Zhongli",
+    element: "Geo",
+    image: "https://example.com/zhongli.jpg",
+    altText: "Zhongli, the Geo Archon",
+  },
+  {
+    name: "Diluc",
+    element: "Pyro",
+    image: "https://example.com/diluc.jpg",
+    altText: "Diluc, the owner of Dawn Winery",
+  },
+  {
+    name: "Fischl",
+    element: "Electro",
+    image: "https://example.com/fischl.jpg",
+    altText: "Fischl, the Prinzessin der Verurteilung",
+  },
+  {
+    name: "Mona",
+    element: "Hydro",
+    image: "https://example.com/mona.jpg",
+    altText: "Mona, the Astrologist",
+  },
+  {
+    name: "Xingqiu",
+    element: "Hydro",
+    image: "https://example.com/xingqiu.jpg",
+    altText: "Xingqiu, the 11th of the Feiyun Commerce Guild",
+  },
+  {
+    name: "Xinyan",
+    element: "Pyro",
+    image: "https://example.com/xinyan.jpg",
+    altText: "Xinyan, the rock 'n' roll musician",
+  },
+  {
+    name: "Keqing",
+    element: "Electro",
+    image: "https://example.com/keqing.jpg",
+    altText: "Keqing, the Yuheng of the Liyue Qixing",
+  },
+  {
+    name: "Ganyu",
+    element: "Cryo",
+    image: "https://example.com/ganyu.jpg",
+    altText: "Ganyu, the half-qilin Adeptus",
+  },
+  {
+    name: "Raiden Shogun",
+    element: "Electro",
+    image: "https://example.com/raiden_shogun.jpg",
+    altText: "Raiden Shogun, the Electro Archon",
+  },
+];
+
+export {genshinCharacters};
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,4 @@
 import '../css/style.css'; //to leave and enter the folder
+import { genshinCharacters } from './genshin';
+
+console.log(genshinCharacters);
