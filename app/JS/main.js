14e33de15d01d708376baa10c20eef4739a769ef
hambergss <144480774+hambergss@users.noreply.github.com>
2024-11-06 13:58:46 -0500

    b

--- app/JS/main.js
+++ app/JS/main.js
@@ -2,3 +2,21 @@ import '../css/style.css'; //to leave and enter the folder
 import { genshinCharacters } from './genshin';
 
 console.log(genshinCharacters);
+
+const card = document.getElementById("characters-list");
+
+genshinCharacters.forEach((character)) => {
+    const genshinHTML = `
+        <div class="character-item">
+        <h2>${character.name}</h2>
+        <p>Star: ${character.star}</p>
+        <p>Banner: ${character.banner}</p>
+        <p>Element: ${character.element}</p>
+        <p>Weapon: ${character.weapon}</p>
+        <p>Type: ${character.type}</p>
+        <p>Tier: ${character.tier}</p>
+        <p>Image: ${character.image}</p>
+        </div>
+    `;
+    charactersListContainer.insertAdjacentHTML("beforeend", genshinHTML);
+}
